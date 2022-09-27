d55d6721e0ee5a229dbeee53580954e440f3c4a7
jochemvs <114492359+jochemvs@users.noreply.github.com>
2022-09-27 15:08:21 +0200

    notDead defined

--- maingame.py
+++ maingame.py
@@ -1,6 +1,6 @@
 print("Welcome to[insert game name here]!")
 name = input("What is your name?")
 print(f"Hello, {name}! Nice to meet you! Are you ready to go on an expedition?")
-
+notDead = True
 while notDead == True:
     print("this is working")
