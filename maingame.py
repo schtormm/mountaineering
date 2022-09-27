41896819b0709d517670ffe1e10e21fbb6da3c78
jochemvs <114492359+jochemvs@users.noreply.github.com>
2022-09-27 15:10:48 +0200

    mountaineering

--- maingame.py
+++ maingame.py
@@ -1,6 +1,6 @@
 notDead = True
 
-print("Welcome to[insert game name here]!")
+print("Welcome to the start point of Mountaineering!")
 name = input("What is your name?")
 print(f"Hello, {name}! Nice to meet you! Are you ready to go on an expedition?")
 
